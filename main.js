a092bb7ee3ea09901bda2b6c52deb81686386532
spacehiker <filip.osin@gmail.com>
2020-05-20 21:21:11 +0200

    pwa setup

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
-const displayText = document.querySelector('.display');
-const numberButton = document.querySelectorAll('.button__num--js');
-const operantionButton = document.querySelectorAll('.button__op--js');
-const resultButton = document.querySelector('.button__sum--js');
-const clearButton = document.querySelector('.button__clr--js');
+window.onload = () => {
+    'use strict';
+
+    if ('serviceWorker' in navigator) {
+        navigator.serviceWorker
+            .register('./sw.js');
+    }
+}
+
+const displayText = document.querySelector(".display");
+const numberButton = document.querySelectorAll(".button__num--js");
+const operantionButton = document.querySelectorAll(".button__op--js");
+const resultButton = document.querySelector(".button__sum--js");
+const clearButton = document.querySelector(".button__clr--js");
 
 let previousNumber = 0;
 let currentNumber = 0;
@@ -14,8 +23,8 @@ function Round(n, k) {
 }
 
 function clear() {
-    display('0');
-    console.log('clear');
+    display("0");
+    console.log("clear");
     previousNumber = 0;
     currentNumber = 0;
     operationType = null;
@@ -26,21 +35,21 @@ function result() {
     current = parseFloat(currentNumber);
     if (prev != 0) {
         switch (operationType) {
-            case '+':
+            case "+":
                 currentNumber = prev + current;
                 break;
-            case '-':
+            case "-":
                 currentNumber = prev - current;
                 break;
-            case '*':
+            case "*":
                 currentNumber = prev * current;
                 break;
-            case '/':
+            case "/":
                 currentNumber = prev / current;
                 break;
             default:
                 break;
-        };
+        }
         console.log(`result: ${currentNumber}`);
         display(Round(currentNumber, 8));
     }
@@ -50,8 +59,7 @@ function display(number) {
     displayText.value = number;
 }
 
-clearButton.addEventListener('click', clear);
-
+clearButton.addEventListener("click", clear);
 
 /*
 zmienna.forEach(funkcja);
@@ -80,29 +88,24 @@ function dodawanie(button) {
 
 */
 
-numberButton.forEach(button => {
-    button.addEventListener('click', () => {
+numberButton.forEach((button) => {
+    button.addEventListener("click", () => {
         if (currentNumber == 0) currentNumber = button.value;
         else currentNumber += button.value;
         console.log(`button: ${button.value}`);
         display(currentNumber);
         console.log(`value: ${currentNumber}`);
+    });
+});
 
-    })
-})
-
-
-
-operantionButton.forEach(button => {
-    button.addEventListener('click', () => {
-
+operantionButton.forEach((button) => {
+    button.addEventListener("click", () => {
         previousNumber = currentNumber;
         currentNumber = 0;
         console.log(`previous: ${previousNumber}`);
         operationType = button.value;
         console.log(`operation: ${operationType}`);
+    });
+});
 
-    })
-})
-
-resultButton.addEventListener('click', result);
+resultButton.addEventListener("click", result);
